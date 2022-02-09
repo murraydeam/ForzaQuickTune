449a3d511bbaea4b94c57f9b7ed36b591b42c236
deand <Deandremurray22@gmail.com>
2022-02-09 14:12:43 -0500

    Take75

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,9 @@
-import logo from './SVG/logo.svg';
-import './App.css';
-import ButtonAppBar from './components/NavBar';
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-      <div>
-      <ButtonAppBar />
-      </div>
+    <div className="Container">
+      <Header />
     </div>
   );
 }
