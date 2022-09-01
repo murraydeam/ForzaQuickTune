b97a5a3f551cfaefb8716b68b38af36d2117b727
murraydeam <73445952+murraydeam@users.noreply.github.com>
2022-09-01 13:40:29 -0400

    Button in the header now navigates to the tune section when selected. The button also has new sizing relative to the screen.A flex gap has also been added to the H2 for each of the sliders.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,19 +7,19 @@ const Header = () => {
       <div className="header-bg">
         <div className="header-button">
           <Button
+            href="#Tune"
             variant="contained"
             sx={{
-              
               height: 100,
               margin: {
-                xs:4,
-                sm:2,
-                md:2,
-                lg:4,
-                xl:2,
+                xs: 4,
+                sm: 8,
+                md: 8,
+                lg: 16,
+                xl: 16,
               },
               width: {
-                xs: 80,
+                xs: 100,
                 sm: 120,
                 md: 180,
                 lg: 240,
