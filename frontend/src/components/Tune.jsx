adec5bf66d1758df3acc493b14865c76b781810f
murraydeam <73445952+murraydeam@users.noreply.github.com>
2022-09-01 13:41:13 -0400

    Tune.jsx has been added after having the name changed.

---
+++ frontend/src/components/Tune.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container } from "@mui/material";
+import { Box } from "@mui/material";
+import Sliders from "./Sliders";
+
+const Tune = () => {
+  return (
+    <Container className="tune-content" id="Tune">
+      <Box sx={{ my: 6 }}>
+        <h1>
+          Below, select your vehicles weight and weight bias to begin getting
+          your base tune!
+        </h1>
+      </Box>
+      <Box sx={{ my: 12 }}>
+        <Sliders />
+      </Box>
+    </Container>
+  );
+};
+
+export default Tune;
