c8cc982d99b2bcfdfb73befabb5714fb9c4010d5
murraydeam <73445952+murraydeam@users.noreply.github.com>
2022-09-01 13:20:02 -0400

    Issue with button containment has been resolved in header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,37 @@
+import { Container } from "@mui/material";
 import { Button, Box } from "@mui/material";
 
 const Header = () => {
   return (
-    <Box>
-      <Button
-        variant="contained"
-        sx={{
-          height: 200,
-          width: {
-            xs: 60,
-            sm: 120,
-            md: 180,
-            lg: 240,
-            xl: 300,
-          },
-          
-        }}
-      >
-        Begin Tunging
-      </Button>
-    </Box>
+    <div className="header-content">
+      <div className="header-bg">
+        <div className="header-button">
+          <Button
+            variant="contained"
+            sx={{
+              
+              height: 100,
+              margin: {
+                xs:4,
+                sm:2,
+                md:2,
+                lg:4,
+                xl:2,
+              },
+              width: {
+                xs: 80,
+                sm: 120,
+                md: 180,
+                lg: 240,
+                xl: 300,
+              },
+            }}
+          >
+            Begin Tuning
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 };
 
